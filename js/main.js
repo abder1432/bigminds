28249d0d47eae0a648c7a55f6be31d9be9473de9
firoud <firoud@mits-sa.com>
2021-02-28 23:58:13 +0300

    Initial coomit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        dots: true
+    });
+  });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
